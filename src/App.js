9ee8980a362d8e6c29c1afd776d9e9c53cff826b
twalk2 <2twalk2@gmail.com>
2019-10-09 15:18:16 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/linkedin-logo-small.png";
+import blacklogo from "./images/linkedin-black-logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="header-border"></div>
+      <div className="header">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="welcome">
+        <h1>Welcome Back</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Don't miss your next opportunity. Sign in to stay updated on your
+          professional world.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      </div>
+      <form>
+        <input placeholder="Email or Phone" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign in</button>
+      </form>
+      <div className="help">
+        <a href="#" id="forgot" className="help-links">
+          Forgot password?
         </a>
-      </header>
+        <p>
+          New to LinkedIn?{" "}
+          <a href="#" id="join" className="help-links">
+            Join now
+          </a>
+        </p>
+      </div>
+      <div className="footer">
+        <div className="footer-left-side">
+          <img src={blacklogo} alt="black logo" />
+          &copy; 2019
+        </div>
+        <a href="#">User Agreement</a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Community Guidelines</a>
+        <a href="#">Cookie Policy</a>
+        <a href="#">Copyright Policy</a>
+        <a href="#">Send Feedback</a>
+      </div>
     </div>
   );
 }
