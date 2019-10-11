9ae8440777316ee6863f077589a6a8311f9130b8
twalk2 <2twalk2@gmail.com>
2019-10-11 09:54:14 -0600

    added focus behavior and functional show/hide button

--- src/App.js
+++ src/App.js
@@ -1,54 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./images/linkedin-logo-small.png";
 import blacklogo from "./images/linkedin-black-logo.png";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header-border"></div>
-      <div className="header">
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="welcome">
-        <h1>Welcome Back</h1>
-        <p>
-          Don't miss your next opportunity. Sign in to stay updated on your
-          professional world.
-        </p>
-      </div>
-      <form>
-        <input placeholder="Email or Phone" type="email" />
-        <input placeholder="Password" type="password" />
-        <button type="submit">Sign in</button>
-      </form>
-      <div className="help">
-        <a href="#" id="forgot" className="help-links">
-          Forgot password?
-        </a>
-        <p>
-          New to LinkedIn?{" "}
-          <a href="#" id="join" className="help-links">
-            Join now
-          </a>
-        </p>
-      </div>
-      <div className="footer">
-        <div className="footer-left-side">
-          <img src={blacklogo} alt="black logo" />
-          &copy; 2019
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isToggleOn: true
+    };
+  }
+
+  PasswordChanger = () => {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header-border"></div>
+        <div className="header">
+          <img src={logo} alt="logo" />
         </div>
-        <a href="#">User Agreement</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Community Guidelines</a>
-        <a href="#">Cookie Policy</a>
-        <a href="#">Copyright Policy</a>
-        <a href="#">Send Feedback</a>
+        <div className="welcome">
+          <h1>Welcome Back</h1>
+          <p>
+            Don't miss your next opportunity. Sign in to stay updated on your
+            professional world.
+          </p>
+        </div>
+        <form>
+          <div className="input-wrapper">
+            <input type="text" required />
+            <span class="floating-label">Email or Phone</span>
+          </div>
+          <div className="input-wrapper">
+            <input
+              type={this.state.isToggleOn ? "password" : "text"}
+              required
+            />
+            <span className="floating-label">Password</span>
+            <button
+              type="button"
+              className="show-button"
+              onClick={this.PasswordChanger}
+            >
+              {this.state.isToggleOn ? "Show" : "Hide"}
+            </button>
+          </div>
+          <button type="submit">Sign in</button>
+        </form>
+        <div className="help">
+          <a href="#" id="forgot" className="help-links">
+            Forgot password?
+          </a>
+          <p>
+            New to LinkedIn?{" "}
+            <a href="#" id="join" className="help-links">
+              Join now
+            </a>
+          </p>
+        </div>
+        <div className="footer">
+          <div className="footer-left-side">
+            <img src={blacklogo} alt="black logo" />
+            &copy; 2019
+          </div>
+          <a href="#">User Agreement</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Community Guidelines</a>
+          <a href="#">Cookie Policy</a>
+          <a href="#">Copyright Policy</a>
+          <a href="#">Send Feedback</a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 // App.js page
-
-export default App;
